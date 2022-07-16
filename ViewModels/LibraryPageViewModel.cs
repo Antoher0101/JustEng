02e7b32f9f9d464901d1b2472ff9922da7ac3124
Antoher0101 <firelessball@yandex.ru>
2022-07-16 05:32:49 +0500

    Added styles for the Library page

--- ViewModels/LibraryPageViewModel.cs
+++ ViewModels/LibraryPageViewModel.cs
@@ -1,4 +1,8 @@
 ﻿using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using JustEng.Infrastructure.Commands;
 using JustEng.JustEngDAL.Entities;
 using JustEng.JustEngInterfaces;
 using JustEng.ViewModels.Base;
@@ -12,6 +16,28 @@ namespace JustEng.ViewModels
 		private Library[] _libraries;
 
 		public Library[] Libraries { get => _libraries; set => Set(ref _libraries, value); }
+
+		#region commands
+
+		#region Command OpenLibraryCommand - Open choosen library
+
+		private ICommand _OpenLibraryCommand;
+
+		public ICommand OpenLibraryCommand => _OpenLibraryCommand
+			??= new RelayCommand(OnOpenLibraryCommandExecuted, CanOpenLibraryCommandExecute);
+
+		/// <summary>Проверка возможности выполнения - Open choosen library</summary>
+		private bool CanOpenLibraryCommandExecute(object p) => true;
+
+		/// <summary>Логика выполнения - Open choosen library</summary>
+		private void OnOpenLibraryCommandExecuted(object p)
+		{
+			
+		}
+
+		#endregion
+
+		#endregion
 
 		public LibraryPageViewModel(IRepository<Library> LibrariesRepository)
 		{
---
+++ Views/UserControls/LibrariesControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JustEng.JustEngDAL.Entities;
+
+namespace JustEng.Views.UserControls
+{
+	/// <summary>
+	/// Логика взаимодействия для LibrariesControl.xaml
+	/// </summary>
+	public partial class LibrariesControl : UserControl
+	{
+		public LibrariesControl()
+		{
+			InitializeComponent();
+		}
+
+		#region LibrariesSource : Library[] - Массив типа Library
+
+		/// <summary>Массив типа Library</summary>
+		public static readonly DependencyProperty LibrariesSourceProperty =
+			DependencyProperty.Register(
+				nameof(LibrariesSource),
+				typeof(Library[]),
+				typeof(LibrariesControl),
+				new PropertyMetadata(default(Library[])));
+
+		/// <summary>Массив типа Library</summary>
+		//[Category("")]
+		[Description("Массив типа Library")]
+		public Library[] LibrariesSource { get => (Library[])GetValue(LibrariesSourceProperty); set => SetValue(LibrariesSourceProperty, value); }
+
+		#endregion
+	}
+}
